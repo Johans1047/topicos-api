a03a6296784a967400a55c602033828f69da15e9
Jonathan Salazar <jonyalex1047@gmail.com>
2025-06-01 19:36:10 -0500

    title changed

--- client/src/views/ListView.tsx
+++ client/src/views/ListView.tsx
@@ -34,7 +34,8 @@ export default function ListView(): React.JSX.Element {
                 <Alert daisyColor={alert.daisyColor} message={alert.message} />
             }
             <div className="w-full max-w-5xl mx-auto p-4">
-                <h1 className="text-3xl font-semibold my-4">Simple To-do App</h1>
+                {/* <h1 className="text-3xl font-semibold my-4">Simple To-do App</h1> */}
+                <h1 className="text-3xl font-semibold my-4">This simple App has been changed</h1>
                 <div className="overflow-x-auto rounded-box shadow-md bg-base-100 mb-6">
                     <table className="table">
                         <thead>
